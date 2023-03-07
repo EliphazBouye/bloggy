58a7c8999fb770b83a569e35815b953f37e6165a
Eliphaz BOUYE <eliphazbouye@gmail.com>
2023-03-07 12:13:41 +0000

    v1 get query params on route

--- src/Route.php
+++ src/Route.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace Bloggy;
+
+use ReflectionFunction;
+
 /**
  * Route class
  */
 class Route{
     static private $routes = [];
-
+    static private $query_params;
     /**
      * get handle for get method
      * Params
@@ -16,6 +19,7 @@ class Route{
      */
     static public function get(string $uri,callable | array $handler, string $name = "" ) {
         // TODO make some verification for validate uri
+
         array_push(self::$routes, array('uri' => $uri, 'handler' => $handler, 'method' => 'GET'));
     }
 
@@ -24,15 +28,24 @@ class Route{
         if (is_callable(self::$routes[$result_search]['handler'])) {
             return self::$routes[$result_search]['handler']();
         } else if (gettype(self::$routes[$result_search]['handler']) === 'array') {
+            $uri = self::$routes[$result_search]['uri'];
             $handler = explode('@', self::$routes[$result_search]['handler'][0]);
             $controller = $handler[0];
             $method  = $handler[1];
-            // var_dump("controller class : $controller");
-            self::init_class($controller, $method);
+
+            if (str_contains($uri, ":")) {
+                $param = explode(':', $uri);
+                if (array_key_exists($param[1], $_GET) != null) {
+                    self::$query_params = $_GET[$param[1]];
+                    self::init_class($controller, $method, self::$query_params);
+                } else {
+                    echo "Key $param[1] is required";
+                }
+            }
         }
     }
 
-    static private function init_class($controller, $method) {
+    static private function init_class($controller, $method, $param) {
         $controller_folder = __DIR__ . "/controllers";
         $class_file = $controller_folder . '/' . $controller . '.php';
 
@@ -41,6 +54,12 @@ class Route{
 
             $class_namespace = __NAMESPACE__ . '\\Controller\\' . $controller;
             $class = new $class_namespace();
+            
+            $reflection = new \ReflectionMethod($class, $method);
+            if ($reflection->getNumberOfRequiredParameters() > 0) {
+                return $class->$method($param);
+            }
+
             return $class->$method();
         } else {
             trigger_error("Controller class don't exist", E_USER_ERROR);
